b72cd0868e792ea950b3a2d8839e147cb4f7c19b
farang <artemiavorskyi@gmail.com>
2023-11-12 22:58:01 -0100

    update: number types

---
+++ basics/data-types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	// numbers
+	int income = 2000000;
+	short year = 2023;
+	long yearsAgo = 1000000;
+	long long bytes = 999999999999;
+	
+	float width = 23.35;
+	double amount = 12.4545456457;
+	long double fractionOfDistance = 1.123124234354367567658;
+
+	return 0;
+}
