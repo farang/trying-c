3d7ff4f9d239c4f268a3eaeacda2be084e3e7059
farang <artemiavorskyi@gmail.com>
2023-11-16 10:33:07 -0100

    update: while loop

--- basics/loops.c
+++ basics/loops.c
@@ -3,13 +3,21 @@
 int ids[] = {1,2,3,4,5,6,7};
 
 int main() {
-	// for loop
-	int count = sizeof(ids) / sizeof(ids[0]);
+    // for loop
+    int count = sizeof(ids) / sizeof(ids[0]);
 
-	for (int i = 0; i < count; i++) {
-		printf("%d", ids[i]);
-	}
+    for (int i = 0; i < count; i++) {
+        printf("for %d\n", ids[i]);
+    }
 
-	return 0;	
+    // while loop
+    int i = 1;
+
+    while(i < 8) {
+        printf("while %d\n", i);
+	i++;
+    }
+
+    return 0;	
 }
 
