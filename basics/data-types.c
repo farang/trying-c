b96cf295fb1f646a275a8d180bfb07c1bf491623
farang <artemiavorskyi@gmail.com>
2024-01-27 22:17:03 +0100

    refactoring: data types and loops files formatting

--- basics/data-types.c
+++ basics/data-types.c
@@ -4,15 +4,16 @@
 void add(int *n);
 
 // typedef
-typedef int BIG_INT; 
+typedef int BIG_INT;
 
-int main() {
+int main()
+{
 	// numbers
 	int income = 2000000;
 	short year = 2023;
 	long yearsAgo = 1000000;
 	long long bytes = 999999999999;
-	
+
 	float width = 23.35;
 	double amount = 12.4545456457;
 	long double fractionOfDistance = 1.123124234354367567658;
@@ -36,6 +37,7 @@ int main() {
 }
 
 // functions
-void add(int *n) {
+void add(int *n)
+{
 	*n = *n + 1;
 }
--- basics/loops.c
+++ basics/loops.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
 
-int ids[] = {1,2,3,4,5,6,7};
+int ids[] = {1, 2, 3, 4, 5, 6, 7};
 
-int main() {
+int main()
+{
     // for loop
     int count = sizeof(ids) / sizeof(ids[0]);
 
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++)
+    {
         printf("for %d\n", ids[i]);
     }
 
     // while loop
     int i = 1;
 
-    while(i < 8) {
+    while (i < 8)
+    {
         printf("while %d\n", i);
-	i++;
+        i++;
     }
 
     // do while
     int i2 = 1;
 
-    do {
-	printf("do while %d\n", i2);
-    	i2++;
+    do
+    {
+        printf("do while %d\n", i2);
+        i2++;
     } while (i2 < 8);
 
     int i3 = 0;
 
-    do {
-    	printf("executed at least once");
+    do
+    {
+        printf("executed at least once");
     } while (i3 > 0);
 
-    return 0;	
+    return 0;
 }
-
